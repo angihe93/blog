ca696b4bf01905a85617446fe7256567cc32dda0
angihe93 <angelhe903@gmail.com>
2025-06-08 13:12:29 -0400

    finish post

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`Hi there! I am a software developer and quant research developer based in NYC.
-        As a recent grad, I hope to gain project experiences and further my skills in these domains.
+        As a recent grad, I hope to gain experiences and further my skills in these domains.
         I have worked on full stack development, as well as quantitative and data heavy projects.
         My stack so far: Python, JavaScript/TypeScript, React, C/C++, R, SQL, Postgres, GCP, AWS, Github CI/CD.`}
       </p>
